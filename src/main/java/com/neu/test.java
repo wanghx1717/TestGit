61a44c0e91bcae821b553a690a3c515d484ddd98
wanghx1717 <wanghx1717@163.com>
2021-05-23 19:52:03 +0800

    wanghx0717 first commit

---
+++ src/main/java/com/neu/test.java
@@ -0,0 +1,13 @@
+package com.neu;
+
+/**
+ * @author 王泓勋
+ * @date 2021/5/23   7:20 下午
+ */
+
+
+public class test {
+    public static void main(String[] args) {
+        System.out.println("hello world");
+    }
+}
---
+++ src/main/java/com/neu/test2.java
@@ -0,0 +1,13 @@
+package com.neu;
+
+/**
+ * @author 王泓勋
+ * @date 2021/5/23   7:27 下午
+ */
+
+
+public class test2 {
+    public static void main(String[] args) {
+        System.out.println(1);
+    }
+}
