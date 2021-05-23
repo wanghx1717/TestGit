cb136f610623f0288568b03f2955e9c1a7fede07
wanghx1717 <wanghx1717@163.com>
2021-05-23 20:07:30 +0800

    wanghx0717 third commit

--- src/main/java/com/neu/test.java
+++ src/main/java/com/neu/test.java
@@ -10,5 +10,6 @@ public class test {
     public static void main(String[] args) {
         System.out.println("hello world");
         System.out.println("aaaa");
+        System.out.println("bbbb");
     }
 }
